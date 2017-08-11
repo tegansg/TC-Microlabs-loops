36fa4ec2946f1efe5b6ceddbed2e60b949b4a61b
Tegan Salter-George <tsaltergeorge@hotmail.com>
2017-08-11 09:29:55 +0100

    Finished

--- src/main/java/io/zipcode/microlabs/loops/Loops.java
+++ src/main/java/io/zipcode/microlabs/loops/Loops.java
@@ -1,14 +1,109 @@
 package io.zipcode.microlabs.loops;
 
+public class Loops 
+{
 
-public class Loops {
-
-    public static String printArray(String[] input){
+    public static String printArray(String[] input)
+    {
         String response = "*** Output ***";
-        for(int x=0; x< input.length; x++){
+        for(int x=0; x< input.length; x++)
+        {
             response +="\n" + input[x];
         }
         return response;
     }
+  
+    
+    // 2 Last Element of Array
+    public static String lastElement(String[] input)
+    {
+    	int length = input.length;
+    	String output = "*** Output ***\n" +
+    					input[length-1]; // length - 1 is the index of the last element
+    	return output;
+    }
+    
 
+    // 3 Last but One Element of Array
+    public static String lastButOne(String[] input)
+    {
+    	int length = input.length;
+    	String output = "*** Output ***\n"
+    			+ input[length-2]; // length - 2 is the index of the last but one element
+    	return output;
+    }
+    
+    
+    
+    // 4 Reverse and Array
+    public static String reverse(String[] input)
+    {
+        String response = "*** Output ***";
+        int length = input.length;
+        
+        for(int x=1; x<=length; x++)
+        {
+        	String ex = input[length-x];
+            response +="\n"+ex;
+        }
+        return response;
+    }
+   
+    // 5 is Palindrome
+
+    public static Boolean isPalindrome(String[] input)
+    {
+    	int length = input.length;
+    	for(int x=0; x<=length/2-1; x++)
+    	{
+    		if(input[x] != input[length - x - 1])
+    		{
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    // 6 Consecutive Duplicates
+    public static String compress(int[] input)
+    {
+    	int length = input.length;
+    	String response = "";
+    	
+    	response += input[0]; // add first element from input into response
+
+    	for(int x=1; x<length; x++)
+    	{
+    		int before = x - 1;
+    		if(input[before] != input[x])
+    		{
+    			response += "\n" + input[x];
+    		}
+    	}
+    	return response;
+    }
+    
+    
+    
+    // 7 Pack Duplicates
+    public static String pack(char[] input)
+    {
+    	int length = input.length;
+    	String response = "";
+    
+    	for(int x=0; x<length-1; x++)
+    	{
+    		response += input[x];
+    		
+    		if (input[x] != input[x+1]) 
+    		{
+    			response += ", ";
+    		}		
+    	}			
+    	
+    	response += input[length-1]; // add last element from input into response
+    	
+    	return response;   	
+    }
+    
 }
--- src/test/java/io/zipcode/microlabs/loops/LoopsTest.java
+++ src/test/java/io/zipcode/microlabs/loops/LoopsTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 public class LoopsTest {
 
     @Test
-    public void printArrayTest(){
+    public void printArrayTest()
+    {
         // :Given
         String [] breakfast = {"Sausage", "Eggs", "Beans",
                 "Bacon", "Tomatoes", "Mushrooms"};
@@ -24,4 +25,139 @@ public class LoopsTest {
         //:Then
         Assert.assertEquals(expected, actual);
     }
+    
+    @Test
+    public void lastElementTest()
+    {
+    	
+        // :Given
+        String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\nMushrooms";
+       
+    	
+        //:when
+        String actual =Loops.lastElement(breakfast);
+
+        //:Then
+        Assert.assertEquals(expected, actual);
+ 
+    }
+    
+
+    @Test
+    public void lastButOneTest()
+    {
+        // :Given
+        String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\nTomatoes";
+    	
+        //:when
+        String actual =Loops.lastButOne(breakfast);
+
+        //:Then
+        Assert.assertEquals(expected, actual);	
+    	
+    	
+ 
+    }
+    
+    
+    
+    @Test
+    public void reverseTest()
+    {
+    	
+        // :Given
+        String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\nMushrooms\nTomatoes\nBacon\nBeans\nEggs\nSausage";
+    	
+        //:when
+        String actual =Loops.reverse(breakfast);
+
+        //:Then
+        Assert.assertEquals(expected, actual);
+    	
+ 
+    }
+    
+    @Test
+    public void isPalindromeTrueTest()
+    {
+    	
+        // :Given
+    	String [] palindromic = {"Sausage", "Eggs", "Beans",
+    			"Beans", "Eggs", "Sausage"};
+    	
+    	
+    	Boolean expectedTrue = true;
+
+        //:when
+    	
+    	Boolean actualTrue = Loops.isPalindrome(palindromic);
+        
+    	//:Then
+        Assert.assertEquals(expectedTrue, actualTrue);
+
+    	
+ 
+    }
+    
+    @Test
+    public void isPalindromeFalseTest()
+    {
+    	
+        // :Given    	
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+    			"Bacon", "Tomatoes", "Mushrooms"};
+    	
+    	Boolean expectedFalse = false;
+        //:when
+    	
+    	Boolean actualFalse = Loops.isPalindrome(breakfast);
+        
+    	//:Then
+        Assert.assertEquals(expectedFalse, actualFalse);
+    	
+ 
+    }
+    
+    @Test
+    public void compressTest()
+    {
+    	
+        // :Given
+    	int[] nums = {1,1,3,3,3,2,2,2,1,1,1,1,4,4,4,4};
+    	String expected = "1\n3\n2\n1\n4";
+       
+
+        //:When
+    	String actual = Loops.compress(nums);
+    	
+    	//:Then
+        Assert.assertEquals(expected, actual);
+    	
+ 
+    }
+    
+    
+    @Test
+    public void packTest()
+    {
+    	
+        // :Given
+    	 char [] letters = {'a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'};
+    	 String expected = "aaaa, b, cc, aa, d, eeee";
+    	 
+        //:when
+    	 String actual = Loops.pack(letters);
+
+        //:Then
+         Assert.assertEquals(expected, actual);
+   
+ 
+    }
+    
 }
